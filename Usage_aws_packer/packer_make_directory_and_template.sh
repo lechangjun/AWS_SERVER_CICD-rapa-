46c9c005c120204e810fe6efda9a38587840c804
lechangjun <68671394+lechangjun@users.noreply.github.com>
2021-05-25 15:56:09 +0900

    Add files via upload

---
+++ Usage_aws_packer/packer_make_directory_and_template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cyan='\033[0;36m'
+green='\033[0;32m'
+lightgreen='\e[0;32m'
+white='\e[0;37m'
+red='\e[0;31m'
+yellow='\033[0;33m'
+blue='\033[0;34m'
+orange='\e[38;5;166m'
+
+
+mkdir packer_tutorial
+cd packer_tutorial
+vi aws-ubuntu.pkr.hcl
